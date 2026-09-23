44d5eec15d2566befd73e3f9c093008f420714c1
rickysurya <rickysuryaa10@gmail.com>
2026-09-23 23:43:08 +0700

    initial commit

---
+++ src/main/java/com/rickysurya/commonplace/CommonplaceApplication.java
@@ -0,0 +1,13 @@
+package com.rickysurya.commonplace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonplaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonplaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rickysurya/commonplace/CommonplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rickysurya.commonplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
